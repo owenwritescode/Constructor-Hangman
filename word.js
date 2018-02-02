9de80948d3297d755029ac514df079a99c009185
Owen Geoghegan <fyou@Owens-MacBook-Pro.local>
2018-02-01 19:25:38 -0800

    adding word.js

---
+++ word.js
@@ -0,0 +1,42 @@
+var letter = require('./letter.js');
+
+function myWord(target) {
+  this.target = target;
+  this.lets = [];
+  this.found = false;
+
+  this.getLet = function() {
+    for var(i = 0; i < this.length; i++) {
+      this.lets.push( new letter(this.target[i]));
+    }
+  };
+
+  this.findWord = function() {
+    this.found = this.lets.every(function(currentLetter) {
+      return currentLetter.appear;
+    });
+    return this.found;
+  };
+
+  this.testLetter = function(letterGuessed) {
+    var toBeReturned = 0;
+
+    for(var i = 0; i < this.lets.length; i++) {
+      if(this.lets[i].charac == letterGuessed) {
+        this.lets[i].appear = true;
+        toBeReturned++;
+      }
+    }
+    return toBeReturned;
+  };
+
+  this.renderWord = function() {
+    var string = '';
+    for(var i = 0; i < this.lets.length; i++) {
+      string += this.lets[i].renderLetter();
+    }
+    return string;
+  };
+}
+
+module.exports = myWord;
